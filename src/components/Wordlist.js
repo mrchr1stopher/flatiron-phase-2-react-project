16b3732802eb714f65b910c1e5e18d928a2b167b
Christopher Wiggins <cwiggins610@gmail.com>
2023-10-02 12:54:37 -0400

    created wordlist.js

---
+++ src/components/Wordlist.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+function ShowCards() {
+  return (
+    <div>
+      <h1>Cards</h1>
+    </div>
+  );
+}
+
+export default Wordlist;
